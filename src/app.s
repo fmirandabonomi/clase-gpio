180f573d2667d87fead17a1b902856c6087513bb
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2022-05-06 09:17:18 -0300

    Carga valor #BRR en lugar de sumar 4 a #BSRR

--- src/app.s
+++ src/app.s
@@ -21,16 +21,16 @@
 .set GPIOC_base,(0x40000000 +  0x00010000 + 0x00001000)
 .set CRH,4
 .set PIN_13,(1<<13)
-.set CRH_PIN_13_POS,20
+.set CRH_PIN_13_POS, (13-8)*4
 .set MODO_SALIDA_LENTA, 0b10
 .set CFG_GP_PUSH_PULL, 0
 .set CFG_MASK,0xF
 .set ODR,0x0C
 .set BSRR,0x10
-.set BRR,0x11
+.set BRR,0x14
 defun setup
     push {LR}
-activa_reloj_gpioa:
+activa_reloj_gpioc:
     ldr R2,=RCC_base
     ldr R0,[R2,#APB2ENR] // CARGA
     orrs R0,GPIOC_CLK_ENABLE // MODIFICACIÓN
@@ -38,8 +38,8 @@ activa_reloj_gpioa:
 config_pc13_salida:
     ldr R2,=GPIOC_base
     ldr R0,[R2,#CRH] // CARGA
-    bics R0,#(CFG_MASK << CRH_PIN_13_POS) //MOD. (BORRADO)
-    orrs R0,#((MODO_SALIDA_LENTA | CFG_GP_PUSH_PULL) << CRH_PIN_13_POS) // MOD. (ESTABLECIMIENTO)
+    bics R0,#(CFG_MASK << CRH_PIN_13_POS) //MOD. (Ceros)
+    orrs R0,#((MODO_SALIDA_LENTA | CFG_GP_PUSH_PULL) << CRH_PIN_13_POS) // MOD. (Unos)
     str R0,[R2,#CRH] // ESCRITURA
     bl TimerSysTick_init
     pop {PC}
@@ -52,7 +52,7 @@ defun loop
     ldr R0,[R2,#ODR]
     ands R0,#PIN_13
     beq 0f
-    adds R1,#4
+    movs R1,#BRR
 0:
     movs R0,#PIN_13
     str R0,[R2,R1]
